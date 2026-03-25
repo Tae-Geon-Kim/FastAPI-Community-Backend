1d0353f720f8c31eecc859db891a59d07e31421b
Tae-Geon-Kim <danielkim3276@gmail.com>
2026-03-25 17:51:14 +0900

    Refactor: Boards API HTTPException 도입

--- app/services/boards.py
+++ app/services/boards.py
@@ -1,4 +1,5 @@
 from asyncpg import Connection
+from fastapi import HTTPException
 from app.schemas.boards import CreateBoard, BoardInfo
 from app.schemas.user import UserId
 from app.models.boards import insert_boards_db, certain_user_boards_info
@@ -9,17 +10,11 @@ async def create_boards_services(conn: Connection, data: CreateBoard):
 
     # 게시판 제목이 빈 문자열인 경우
     if not data.title.strip():
-        return {
-            "성공 여부" : "False",
-            "메시지" : "게시판 제목에는 빈 문자열을 사용할 수 없습니다."
-        }
+        raise HTTPException(status_code = 400, detail = "게시판 제목에는 빈 문자열을 사용할 수 없습니다.")
 
     # 게시판 내용이 빈 문자열인 경우
     if not data.content.strip():
-        return {
-            "성공 여부" : "False",
-            "메시지" : "게시판 내용에는 빈 문자열을 사용할 수 없습니다."
-        }
+        raise HTTPException(status_code = 400, detail = "게시판 내용에는 빈 문자열을 사용할 수 없습니다.")
     
     user_num = await login(conn, data)
     # 로그인 성공 시 user_num 에는 사용자의 index 가
@@ -27,10 +22,7 @@ async def create_boards_services(conn: Connection, data: CreateBoard):
 
     # 로그인에 실패한 경우
     if user_num is None:
-        return {
-            "성공 여부" : "False",
-            "메시지" : "로그인 정보를 다시 확인해주세요."
-        }
+        raise HTTPException(status_code = 401, detail = "로그인 정보를 다시 확인해주세요.")
 
     # 게시판을 저장할 때 user_num도 같이 저장
     await insert_boards_db(conn, data, user_num)
@@ -47,20 +39,14 @@ async def boards_info_services(conn: Connection, data: UserId):
 
     # 해당 사용자가 존재 x
     if not user_exist:
-        return {
-            "성공 여부" : "False",
-            "메시지" : "해당 사용자가 존재하지않습니다."           
-        }
+        raise HTTPException(status_code = 404, detail = "해당 사용자가 존재하지않습니다.")
 
     # 해당 사용자가 존재 / 해당 사용자의 전체 게시글 fetch
     rows = await certain_user_boards_info(conn, data.id)
 
     # 해당 사용자가 쓴 게시글이 없는 경우
     if not rows:
-        return {
-            "성공 여부" : "False",
-            "메시지" : f"{data.id}의 등록된 게시글이 없습니다."
-        }
+        raise HTTPException(status_code = 404, detail = f"{data.id}님의 등록된 게시글이 존재하지않습니다.")
     
     board_list = [BoardInfo.model_validate(dict(row)) for row in rows]
     
