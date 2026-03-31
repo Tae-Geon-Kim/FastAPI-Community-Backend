d3dbe19f9afc31f3bcb4d94505a7520b4135815c
Tae-Geon-Kim <danielkim3276@gmail.com>
2026-03-31 13:40:40 +0900

    Refactor: 게시판 모델을 스키마로부터 분리

--- app/models/boards.py
+++ app/models/boards.py
@@ -1,13 +1,12 @@
 # app/schemas/boards.py 에서 스키마 가져와야함
 # depends(get_db)가 빠진 이유: 모델이 FastAPI에 의존하지 않게 해야 코드의 독립성과 재사용성을 유지 가능
 from asyncpg import Connection
-from app.schemas.boards import CreateBoard
 
-async def insert_boards_db(conn: Connection, data: CreateBoard, user_num: int):
+async def insert_boards_db(conn: Connection, title: str, content: str, user_index: int):
 
-	sql = 'INSERT INTO "boards" (title, content, user_index) VALUES ($1, $2, $3)'
+	sql = 'INSERT INTO boards (title, content, user_index) VALUES ($1, $2, $3)'
 
-	return await conn.execute(sql, data.title, data.content, user_num)
+	return await conn.execute(sql, title, content, user_index)
 
 # 특정 유저의 게시판 정보 조회 (INNER JOIN)
 async def certain_user_boards_info(conn: Connection, user_id: str):
